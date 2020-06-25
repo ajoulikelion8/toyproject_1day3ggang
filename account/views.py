f566024fe807d647cbde86aeb54b90c7efdffaff
juyeonblue <jyp0211@ajou.ac.kr>
2020-06-26 01:00:45 +0900

    juyeon: Update views.py

--- account/views.py
+++ account/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 # Create your views here.
 def login(request):
-    return render(request,'account/login.html')
+    if request.method == "post":
+        id = request.post['id']
+        password = request.post['password']
+        user = auth.authenticate(request, id=id, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('mainpage')
+        else:
+            return render(request,'account/login.html', {'error': 'id or password is incorrect'})
+    else:
+        return render(request,'account/login.html')
 
 def signup(request):
+    if request.method == "post":
+        if request.post["password1"] == request.post["password2"]:
+            user =User.objects.create_user(
+                username=request.post["username"],id=request.post["id"],password=request.post["password1"])
+            auth.login(request,user)
+            return redirect('mainpage')
+        return render(request,'account/signup.html')
+    
     return render(request,'account/signup.html')
+
