0b2ae8cccf3db78662ee38a23446edd36c17275d
juyeonblue <jyp0211@ajou.ac.kr>
2020-06-25 21:08:06 +0900

    juyeon: Edit todoilst url

--- todolist/urls.py
+++ todolist/urls.py
@@ -8,5 +8,5 @@ import account.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('content.urls')),
-    path('account',include('account.urls')),
+    path('account/',include('account.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
